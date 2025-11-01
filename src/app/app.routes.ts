3c26d41991ea888201d20b37a617c72a24493c7b
Kailan Dias <kailanlopes1103@gmail.com>
2025-11-01 18:25:28 -0300

    Signup Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'signup', component: SignupComponent },
+    // Rota padrão, redireciona para /usuarios
+    //{ path: '', redirectTo: '/usuarios', pathMatch: 'full' },
+    { path: '', redirectTo: '/signup', pathMatch: 'full' }
+];
---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+}
+
+
